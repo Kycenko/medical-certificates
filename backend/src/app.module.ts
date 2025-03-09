a8e96c369665b8a728693e793eeee30f29ad9dbc
Your Name <you@example.com>
2025-03-09 12:41:02 +0300

    add graphql config & prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'node:path'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+import { PrismaModule } from './prisma.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			playground: false,
+			plugins: [ApolloServerPluginLandingPageLocalDefault()],
+			autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+			sortSchema: true
+		}),
+		PrismaModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
